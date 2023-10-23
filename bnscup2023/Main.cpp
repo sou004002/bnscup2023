b9a4f9d412896c4903e42045cf7b1d34f50b5f34
sou004002 <shuwashuwa_0402@i.softbank.jp>
2023-10-23 16:05:17 +0900

    プロジェクト ファイルを追加します。

---
+++ bnscup2023/Main.cpp
@@ -0,0 +1,54 @@
+﻿# include <Siv3D.hpp>
+
+void Main()
+{
+	Vec2 pos = { Scene::Center().x,200 };
+	float g = 9.81f;
+	float t = 0.0f;
+	bool isJump = false;
+	while (System::Update())
+	{
+		const double delta = (Scene::DeltaTime() * 200);
+		if (!isJump) {
+			t += Scene::DeltaTime();
+		}
+
+		Rect field{ 0,500,800,100 };
+		Circle player{ pos, 20 };
+
+		// 上下左右キーで移動
+		if (KeyLeft.pressed())
+		{
+			pos.x -= delta;
+		}
+
+		if (KeyRight.pressed())
+		{
+			pos.x += delta;
+		}
+
+		// [C] キーが押されたら中央に戻る
+		if (KeyC.down())
+		{
+			pos = Scene::Center();
+		}
+		float vy = (float)t * g;
+		if (player.intersects(field)) {
+			t = 0.0f;
+			vy = 0.0f;
+			isJump = true;
+		}
+		if (KeySpace.pressed() && isJump) {
+			vy -=100.0f;
+			isJump = false;
+		}
+
+
+		pos.y += vy;
+
+		field.draw();
+		player.draw(Palette::Skyblue);
+		Print<< vy;
+
+	}
+}
