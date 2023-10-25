df1007254496f45c698624ffcc4fe4435be36208
竹内柚太朗 <yutaro.takeuchi.7u@stu.hosei.ac.jp>
2023-10-26 01:02:08 +0900

    ごみクラスの作成 mainにたくさん書いたので、競合するかもごめんなさい。。。

---
+++ bnscup2023/Garbage.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+class Garbage
+{
+public:
+	Garbage(int32 s, Texture t, double z):
+		g_p({ 0,0 }), g_s(s), g_texture(t), g_time(z), g_hit(false) {};
+
+	void draw() const;
+	void putpoints(Vec2 pos);
+	void changepos();
+	void changehitter(double t);
+	bool gethitter() const;
+
+private:
+	Vec2 g_p;
+	int32 g_s;
+	Texture g_texture;
+	double g_time;
+	bool g_hit;
+};
